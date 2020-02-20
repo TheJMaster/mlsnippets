1970cc6863034e7c82ffd2479650a9d42aa01b21
Justin Johnson <justinthomasjohnson@gmail.com>
2020-02-20 13:38:13 -0800

    add multi tracking

--- object_detector_app/object_detection_app.py
+++ object_detector_app/object_detection_app.py
@@ -6,6 +6,7 @@ import argparse
 import multiprocessing
 import numpy as np
 import tensorflow as tf
+import random
 
 
 from utils.app_utils import FPS, WebcamVideoStream, HLSVideoStream
@@ -33,11 +34,43 @@ categories = label_map_util.convert_label_map_to_categories(label_map, max_num_c
                                                             use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-track_next_bus = True
+next_box_id = 0
+tracked_box_ids = []
+undetected_counts = []
+colors = []
+
+undetected_threshold = 10 # Number of frames to allow for undetected.
+equality_threshold = 30 # How close we want the edges to be for two boxes to be considered the same.
+
+# Returns true iff the two numbers are within an equality threshold of each other.
+def approx_eq(x, y):
+    return abs(x-y) < equality_threshold
+
+# Returns true iff the list of boxes contains the provided box. We considered two boxes
+# equal if any two of their sides are within some threshold distance of each other.
+def contains_box(box, boxes):
+    for candidate_box in boxes:
+        if sum([approx_eq(box[i], candidate_box[i]) for i in range(len(box))]) >= len(box) / 2:
+            return True
+    return False
+
+# Returns (detected but untracked boxes, undetected but tracked boxes)
+def filter_boxes(tracked_boxes, detected_boxes):
+    detected_untracked_boxes = []
+    for box in detected_boxes:
+        if not contains_box(box, tracked_boxes):
+            detected_untracked_boxes.append(box)
+
+    undetected_tracked_boxes = []
+    for box in tracked_boxes:
+        if not contains_box(box, detected_boxes):
+            undetected_tracked_boxes.append(box)
+
+    return (np.array(detected_untracked_boxes), np.array(undetected_tracked_boxes))
 
 # Sess is used to detect new busses, tracker is used to track existing ones.
 def detect_objects(image_np, sess, detection_graph, tracker):
-    global track_next_bus
+    global next_box_id, tracked_box_ids
 
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
@@ -52,18 +85,19 @@ def detect_objects(image_np, sess, detection_graph, tracker):
     classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-    # Actual detection.
+    # Run initial detection.
     (boxes, scores, classes, num_detections) = sess.run(
         [boxes, scores, classes, num_detections],
         feed_dict={image_tensor: image_np_expanded})
 
     # Remove all instances of non-bus classes.
-    # TODO(justin): Change this to classes == 6
     boxes = np.squeeze(boxes)
     classes = np.squeeze(classes)
     scores = np.squeeze(scores)
 
-    indicies = np.argwhere(classes == 3)
+    # Filter for a particular class.
+    # TODO(justin): Change this to classes == 6 (bus)
+    indicies = np.argwhere(classes == 3) # class 3 == cars
     boxes = np.squeeze(boxes[indicies])
     classes = np.squeeze(classes[indicies]).astype(np.int32)
     scores = np.squeeze(scores[indicies])
@@ -74,38 +108,80 @@ def detect_objects(image_np, sess, detection_graph, tracker):
     classes = np.squeeze(classes[indicies])
     scores = np.squeeze(scores[indicies])
 
+    # Convert all of the boxes to match style from RE-3 tracker.
     height = image_np.shape[0]
     width = image_np.shape[1]
+    detected_boxes = []
+    for box in boxes:
+        if isinstance(box, np.ndarray):
+            detected_boxes.append([box[1]*width, box[0]*height, box[3]*width, box[2]*height])
+    detected_boxes = np.array(detected_boxes)
+    print("[INFO] Detection found {} boxes".format(len(detected_boxes)))
 
-    # Visualize trakcer on array.
-    img = image_np.copy()
-    boxToDraw = None
-    if len(boxes) > 0:
-        if track_next_bus:
-            if isinstance(boxes[0], np.ndarray):
-                h = image_np.shape[0]
-                w = image_np.shape[1]
-                b = boxes[0]
-                box = [b[1]*w, b[0]*h, b[3]*w, b[2]*h]
-                print("box: ", box)
-                boxToDraw = tracker.track('bus', img, box)
-                track_next_bus = False
+    # Find all of the already tracked bounding boxes.
+    tracked_boxes = np.empty(0)
+    if len(tracked_box_ids) > 1:
+        tracked_boxes = tracker.multi_track(tracked_box_ids, image_np)
+    elif len(tracked_box_ids) == 1:
+        tracked_boxes = tracker.track(tracked_box_ids[0], image_np)
+    print("[INFO] Tracker found {} boxes".format(len(tracked_boxes)))
+
+    # Find any boxes that have been detected but not tracked, and vce versa.
+    detected_untracked_boxes, undetected_tracked_boxes = filter_boxes(tracked_boxes, detected_boxes)
+    print("[INFO] {} boxes were detected but untracked".format(len(detected_untracked_boxes)))
+    print("[INFO] {} boxes were undetected but tracked".format(len(undetected_tracked_boxes)))
+
+    # Remove tracked but undetected boxes from tracking.
+    for i, box in reversed(list(enumerate(tracked_boxes))):
+        if np.transpose(box) in undetected_tracked_boxes:
+            undetected_counts[i] = undetected_counts[i] + 1
+            if undetected_counts[i] == undetected_threshold:
+                del tracked_box_ids[i]
+                del colors[i]
+                del undetected_counts[i]
+                print("[INFO] Deleted box {}".format(i))
         else:
-            boxToDraw = tracker.track('bus', img)
-       
-        # If I found a box, draw it.
-        if boxToDraw is not None:
-            cv2.rectangle(image_np,
-                (int(boxToDraw[0]), int(boxToDraw[1])),
-                (int(boxToDraw[2]), int(boxToDraw[3])),
-                [0, 0, 255], 2)
+            undetected_counts[i] = 0
 
-            # If this car left the screen, pick a new one to track.
-            if boxToDraw[0] < 5 or boxToDraw[0] > 1075 or boxToDraw[3] > 715 or boxToDraw[3] < 5 or boxToDraw[3] - boxToDraw[1] < 20 or boxToDraw[2] - boxToDraw[0] < 20:
-                track_next_bus = True
-    elif not track_next_bus:
-        track_next_bus = True
-    
+    # Add untracked but detected boxes to be detected.    
+    init_bounding_boxes = {}
+    for box in detected_untracked_boxes:
+        box_id = "box_{}".format(next_box_id)
+        next_box_id = next_box_id + 1
+        
+        tracked_box_ids.append(box_id)
+        init_bounding_boxes[box_id] = box
+        
+        r = random.uniform(0, 1) * 255
+        color = cv2.cvtColor(np.uint8([[[r, 128, 200]]]), cv2.COLOR_HSV2RGB).squeeze().tolist()
+        colors.append(color)
+
+        undetected_counts.append(0)
+        print("[INFO] Added bounding box for {}".format(box_id))
+
+    # Run tracker again for all boxes.
+    bboxes = np.empty(0)
+    if len(tracked_box_ids) > 1:
+        bboxes = tracker.multi_track(tracked_box_ids, image_np, init_bounding_boxes)
+    elif len(tracked_box_ids) == 1:
+        box_id = tracked_box_ids[0]
+        bboxes = tracker.track(box_id, image_np, init_bounding_boxes[box_id])
+    print("[INFO] {} bounding boxes found by final tracker".format(len(bboxes)))
+
+    # Display detected boxes (helpful for debugging) in gray.
+    for bb, bbox in enumerate(detected_boxes):
+        cv2.rectangle(image_np,
+            (int(bbox[0]), int(bbox[1])),
+            (int(bbox[2]), int(bbox[3])),
+            [204, 204, 204], 2)
+
+    # Display tracked boxes on the image (each in a different color).
+    for bb, bbox in enumerate(bboxes):
+        cv2.rectangle(image_np,
+            (int(bbox[0]), int(bbox[1])),
+            (int(bbox[2]), int(bbox[3])),
+            colors[bb], 2)
+
     return image_np
 
 
@@ -143,8 +219,10 @@ if __name__ == '__main__':
                         default=480, help='Width of the frames in the video stream.')
     parser.add_argument('-ht', '--height', dest='height', type=int,
                         default=360, help='Height of the frames in the video stream.')
+
+    # TODO: Change back the default number of workers to 2.
     parser.add_argument('-num-w', '--num-workers', dest='num_workers', type=int,
-                        default=2, help='Number of workers.')
+                        default=1, help='Number of workers.')
     parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
                         default=5, help='Size of the queue.')
     args = parser.parse_args()
@@ -187,6 +265,6 @@ if __name__ == '__main__':
     print('[INFO] elapsed time (total): {:.2f}'.format(fps.elapsed()))
     print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
 
-    pool.terminate()
+    # pool.terminate()
     video_capture.stop()
     cv2.destroyAllWindows()
