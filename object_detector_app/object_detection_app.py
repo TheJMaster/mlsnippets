85a34f15b678faca614e8a63991ad2d972159cdb
Justin Johnson <justinthomasjohnson@gmail.com>
2020-03-05 21:46:11 -0800

    make pylint happy

--- object_detector_app/object_detection_app.py
+++ object_detector_app/object_detection_app.py
@@ -1,15 +1,18 @@
-import sys
-import os
-import cv2
-import time
-import argparse
-import multiprocessing
-import numpy as np
-import random
+#!/usr/bin/python3
+"""Bus detection and tracking application."""
 
-from utils.app_utils import FPS, LocalVideoStream, HLSVideoStream
+import argparse
 from multiprocessing import Queue, Pool, Process
+import os
+import random
+import sys
+import time
+
+import numpy as np
 from termcolor import cprint
+
+import cv2
+from utils.app_utils import FPS, LocalVideoStream, HLSVideoStream
 
 CWD_PATH = os.getcwd()
 
@@ -25,22 +28,26 @@ COLORS = []
 UNDETECTED_THRESHOLD = 10 # Number of frames to allow for undetected.
 EQUALITY_THRESHOLD = 30 # How close we want the edges to be for two boxes to be considered the same.
 
-# Returns true iff the two numbers are within an equality threshold of each other.
-def approx_eq(x, y):
-    return abs(x-y) < EQUALITY_THRESHOLD
+def log(info):
+    """Log information to console."""
+    cprint("[INFO] {}".format(info), 'grey', 'on_white')
 
 
-# Returns true iff the list of boxes contains the provided box. We considered two boxes
-# equal if any two of their sides are within some threshold distance of each other.
+def approx_eq(num_one, num_two):
+    """Returns true iff the two numbers are within an equality threshold of each other."""
+    return abs(num_one-num_two) < EQUALITY_THRESHOLD
+
+
 def contains_box(box, boxes):
+    """Returns true iff the list of boxes (approx) contains the provided box."""
     for candidate_box in boxes:
         if sum([approx_eq(box[i], candidate_box[i]) for i in range(len(box))]) >= len(box) / 2:
             return True
     return False
 
 
-# Returns (detected but untracked boxes, undetected but tracked boxes)
 def filter_boxes(tracked_boxes, detected_boxes):
+    """Returns detected but untracked bounding boxes, undetected but tracked bounding boxes."""
     detected_untracked_boxes = []
     for box in detected_boxes:
         if not contains_box(box, tracked_boxes):
@@ -54,12 +61,26 @@ def filter_boxes(tracked_boxes, detected_boxes):
     return (np.array(detected_untracked_boxes), np.array(undetected_tracked_boxes))
 
 
-def detect_init():
-    global tf, sess, detection_graph
-    import tensorflow as tf
+def common_boxes(all_boxes):
+    """Returns bounding boxes present in atleast 2 of 3 sets."""
+    boxes = []
+    for box in all_boxes[0]:
+        if contains_box(box, all_boxes[1]) or contains_box(box, all_boxes[2]):
+            boxes.append(box)
+    for box in all_boxes[1]:
+        if not contains_box(box, all_boxes[0]) and contains_box(box, all_boxes[2]):
+            boxes.append(box)
+    return boxes
 
+
+def detect_worker(image_np):
+    # pylint: disable-msg=too-many-locals
+    """Runs object detection on the provided numpy image using a frozen detection model."""
+    import tensorflow as tf  # pylint: disable-msg=import-outside-toplevel
+
+    # Setup tesnorflow session
     detection_graph = tf.Graph()
-    with detection_graph.as_default():
+    with detection_graph.as_default():  # pylint: disable-msg=not-context-manager
         od_graph_def = tf.GraphDef()
         with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
             serialized_graph = fid.read()
@@ -67,8 +88,6 @@ def detect_init():
             tf.import_graph_def(od_graph_def, name='')
             sess = tf.Session(graph=detection_graph)
 
-
-def detection_worker(image_np):
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -76,7 +95,7 @@ def detection_worker(image_np):
     # Each box represents a part of the image where a particular object was detected.
     boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
-    # Each score represent how level of confidence for each of the objects.
+    # Each score represent how level of confidence for each class of the objects.
     scores = detection_graph.get_tensor_by_name('detection_scores:0')
     classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
@@ -86,13 +105,13 @@ def detection_worker(image_np):
         [boxes, scores, classes, num_detections],
         feed_dict={image_tensor: image_np_expanded})
 
-    # Remove all instances of non-bus classes.
+    # Flatten all results for filtering.
     boxes = np.squeeze(boxes)
     classes = np.squeeze(classes)
     scores = np.squeeze(scores)
 
     # Filter for a particular class.
-    # TODO(justin): Change this to classes == 6 (bus)
+    # TODO(justin): change this to classes == 6 (bus)
     indicies = np.argwhere(classes == 3) # class 3 == cars
     boxes = np.squeeze(boxes[indicies])
     classes = np.squeeze(classes[indicies]).astype(np.int32)
@@ -118,12 +137,14 @@ def detection_worker(image_np):
     return detected_boxes
 
 
-def tracker_worker(image_input_q, output_q):
+def track_worker(input_queue, output_queue):
+    """Runs Re3 tracker on images from input queue, passing bounding boxes to output queue."""
     sys.path.insert(1, '/home/jtjohn24/mlsnippets/object_detector_app/re3-tensorflow')
-    from tracker import re3_tracker
+    from tracker import re3_tracker  # pylint: disable-msg=import-outside-toplevel, import-error
 
+    tracker = re3_tracker.Re3Tracker(os.getenv('CUDA_VISIBLE_DEVICES'))
     while True:
-        image_np, init_bounding_boxes = image_input_q.get()
+        image_np, init_bounding_boxes = input_queue.get()
         tracked_boxes = np.empty(0)
         if len(TRACKED_BOX_IDS) > 1:
             tracked_boxes = tracker.multi_track(TRACKED_BOX_IDS, image_np, init_bounding_boxes)
@@ -132,28 +153,16 @@ def tracker_worker(image_input_q, output_q):
             if init_bounding_boxes is not None:
                 init_bounding_box = init_bounding_boxes[TRACKED_BOX_IDS[0]]
             tracked_boxes = tracker.track(TRACKED_BOX_IDS[0], image_np, init_bounding_box)
-        output_q.put(tracked_boxes)
+        output_queue.put(tracked_boxes)
 
 
-# Common boxes returns only the bounding boxes present in 2 of 3 sets.
-def common_boxes(all_boxes):
-    boxes = []
-    for box in all_boxes[0]:
-        if contains_box(box, all_boxes[1]) or contains_box(box, all_boxes[2]):
-            boxes.append(box)
-    for box in all_boxes[1]:
-        if not contains_box(box, all_boxes[0]) and contains_box(box, all_boxes[2]):
-            boxes.append(box)
-    return boxes
+def find_objects(image_np, tracker_input_q, tracker_output_q):
+    # pylint: disable-msg=too-many-locals
+    """Run bus detection using on image, tracking existing buses using input/output queues."""
+    global NEXT_BOX_ID  # pylint: disable-msg=global-statement
 
-def log(s):
-    cprint("[INFO] {}".format(s), 'grey', 'on_white')
-
-# Sess is used to detect new busses, tracker is used to track existing ones.
-def detect_objects(image_np, tracker_input_q, tracker_output_q):
-    # Run and collect multiple detections.
-    pool = multiprocessing.Pool(processes=3, initializer=detect_init)
-    all_detected_boxes = pool.map(detection_worker, [image_np]*3)
+    # Run three seperate object detectors.
+    all_detected_boxes = Pool(processes=3).map(detect_worker, [image_np]*3)
     detected_boxes = common_boxes(all_detected_boxes)
 
     # Find all of the already tracked bounding boxes.
@@ -186,42 +195,41 @@ def detect_objects(image_np, tracker_input_q, tracker_output_q):
         TRACKED_BOX_IDS.append(box_id)
         init_bounding_boxes[box_id] = box
 
-        r = random.uniform(0, 1) * 255
-        color = cv2.cvtColor(np.uint8([[[r, 128, 200]]]), cv2.COLOR_HSV2RGB).squeeze().tolist()
+        hue = random.uniform(0, 1) * 255
+        color = cv2.cvtColor(np.uint8([[[hue, 128, 200]]]), cv2.COLOR_HSV2RGB).squeeze().tolist()
         COLORS.append(color)
 
         UNDETECTED_COUNTS.append(0)
         log("added bounding box for {}".format(box_id))
 
-    # Run tracker again for all boxes.
+    # Run tracker again for all objects to get final bounding boxes.
     tracker_input_q.put((image_np, init_bounding_boxes))
-    bboxes = tracker_output_q.get()
-    log("{} bounding boxes found by final tracker".format(len(bboxes)))
+    bounding_boxes = tracker_output_q.get()
+    log("{} bounding boxes found by final tracker".format(len(bounding_boxes)))
 
-    # Display detected boxes (helpful for debugging) in gray.
-    for bb, bbox in enumerate(detected_boxes):
+    # Display detected boxes in gray.
+    for detected_box in detected_boxes:
         cv2.rectangle(image_np,
-                      (int(bbox[0]), int(bbox[1])),
-                      (int(bbox[2]), int(bbox[3])),
+                      (int(detected_box[0]), int(detected_box[1])),
+                      (int(detected_box[2]), int(detected_box[3])),
                       [204, 204, 204], 2)
 
-    # Display tracked boxes on the image (each in a different color).
-    for bb, bbox in enumerate(bboxes):
+    # Display tracked boxes on the image each in a different color.
+    for idx, bounding_box in enumerate(bounding_boxes):
         cv2.rectangle(image_np,
-                      (int(bbox[0]), int(bbox[1])),
-                      (int(bbox[2]), int(bbox[3])),
-                      COLORS[bb], 2)
+                      (int(bounding_box[0]), int(bounding_box[1])),
+                      (int(bounding_box[2]), int(bounding_box[3])),
+                      COLORS[idx], 2)
 
     return image_np
 
 
-def track_worker(input_q, output_q):
-    # TODO: Add a flag so we can set the CUDA_VISIBLE_DEVICES flag (as they do in re3_tracker.py)
-    # TODO: Do we really need any size > 1 here?
-    t_input_q = Queue(maxsize=3)
-    t_output_q = Queue(maxsize=3)
-    t_worker = Process(target=tracker_worker, args=(t_input_q, t_output_q,))
-    t_worker.start()
+def draw_worker(input_q, output_q):
+    """Detect and track buses from input and save image annotated with bounding boxes to output."""
+    tracker_input_queue = Queue(maxsize=3)
+    tracker_output_queue = Queue(maxsize=3)
+    tracker = Process(target=track_worker, args=(tracker_input_queue, tracker_output_queue,))
+    tracker.start()
     fps = FPS().start()
     while True:
         fps.update()
@@ -229,32 +237,17 @@ def track_worker(input_q, output_q):
         if np.shape(frame) == ():
             continue
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        output_q.put(detect_objects(frame_rgb, t_input_q, t_output_q))
+        output_q.put(find_objects(frame_rgb, tracker_input_queue, tracker_output_queue))
     fps.stop()
-    t_worker.join()
+    tracker.join()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-str', '--stream', dest="stream", action='store', type=str, default=None)
-    parser.add_argument('-src', '--source', dest='video_source', type=int,
-                        default=0, help='Device index of the camera.')
-    parser.add_argument('-wd', '--width', dest='width', type=int,
-                        default=480, help='Width of the frames in the video stream.')
-    parser.add_argument('-ht', '--height', dest='height', type=int,
-                        default=360, help='Height of the frames in the video stream.')
-    parser.add_argument('-p', '--path', dest="video_path", type=str, default=None)
-    parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
-                        default=1, help='Size of the queue.')
-    args = parser.parse_args()
-
-    logger = multiprocessing.log_to_stderr()
-    logger.setLevel(multiprocessing.SUBDEBUG)
-
+def main(args):
+    """Sets up object detection according to the provided args."""
     input_q = Queue(maxsize=args.queue_size)
     output_q = Queue(maxsize=args.queue_size)
-    tracker_proc = Process(target=track_worker, args=(input_q, output_q,))
-    tracker_proc.start()
+    drawer_proc = Process(target=draw_worker, args=(input_q, output_q,))
+    drawer_proc.start()
 
     if args.stream:
         print('Reading from hls stream.')
@@ -274,13 +267,13 @@ if __name__ == '__main__':
     while True:  # fps._numFrames < 120
         frame = video_capture.read()
         input_q.put(frame)
-        t = time.time()
+        start_time = time.time()
 
         output_rgb = cv2.cvtColor(output_q.get(), cv2.COLOR_RGB2BGR)
         cv2.imshow('Video', output_rgb)
         fps.update()
 
-        print('[INFO] elapsed time: {:.2f}'.format(time.time() - t))
+        print('[INFO] elapsed time: {:.2f}'.format(time.time() - start_time))
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -289,6 +282,21 @@ if __name__ == '__main__':
     print('[INFO] elapsed time (total): {:.2f}'.format(fps.elapsed()))
     print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
 
-    tracker_proc.join()
+    drawer_proc.join()
     video_capture.stop()
     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('-str', '--stream', dest="stream", action='store', type=str, default=None)
+    PARSER.add_argument('-src', '--source', dest='video_source', type=int,
+                        default=0, help='Device index of the camera.')
+    PARSER.add_argument('-wd', '--width', dest='width', type=int,
+                        default=480, help='Width of the frames in the video stream.')
+    PARSER.add_argument('-ht', '--height', dest='height', type=int,
+                        default=360, help='Height of the frames in the video stream.')
+    PARSER.add_argument('-p', '--path', dest="video_path", type=str, default=None)
+    PARSER.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
+                        default=1, help='Size of the queue.')
+    main(PARSER.parse_args())
